4fa3ac7a6cd26ac5bad57ba697c07abe1831292d
chris <drikosx323@gmail.com>
2024-03-09 17:20:40 +0200

    Implemented ListNode class for Doubly Linked List

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
+
+    private ListNode head;
+    private ListNode tail;
+    private int length;
+
+    private class ListNode{
+        private int data;
+        private ListNode previous;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data = data;
+        }
+
+    }
+
+    public boolean isEmpty(){
+        return length ==0;
+    }
+
+    public int length(){
+        return length;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
     }
 }
