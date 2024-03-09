3d0a4bda62a816d7d10ac140ba1fb3047638d6e3
chris <drikosx323@gmail.com>
2024-03-09 17:35:49 +0200

    Added insertLast method Added displayForward method Added displayBackward method

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     private ListNode tail;
     private int length;
 
-    private class ListNode{
+    private static class ListNode{
         private int data;
         private ListNode previous;
         private ListNode next;
@@ -23,7 +23,52 @@ public class Main {
         return length;
     }
 
+    public void insertLast(int value){
+        ListNode newNode = new ListNode(value);
+        if (isEmpty()){
+            head = newNode;
+        }else {
+            tail.next = newNode;
+        }
+        newNode.previous = tail;
+        tail = newNode;
+        length++;
+    }
+
+    public void displayForward(){
+        if (head == null){
+            return;
+        }
+        ListNode temp = head;
+        while(temp!=null){
+            System.out.print(temp.data + "-->");
+            temp = temp.next;
+        }
+        System.out.print("null");
+    }
+
+    public void displayBackward(){
+        if (tail == null){
+            return;
+        }
+        ListNode temp = tail;
+        while(temp!=null){
+            System.out.print(temp.data + "-->");
+            temp = temp.previous;
+        }
+        System.out.print("null");
+    }
+
     public static void main(String[] args) {
+
+        Main sll = new Main();
+        sll.insertLast(1);
+        sll.insertLast(10);
+        sll.insertLast(15);
+        sll.insertLast(25);
+
+        //sll.displayForward();
+        sll.displayBackward();
 
 
     }
